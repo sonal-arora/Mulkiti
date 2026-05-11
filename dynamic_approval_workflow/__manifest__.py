2061b25740128796b018b44be68b5430da4c8605
Sonal Arora <sonalarora@Sonals-MacBook-Air.local>
2026-05-11 17:03:54 +0530

    work on leave notification

--- dynamic_approval_workflow/__manifest__.py
+++ dynamic_approval_workflow/__manifest__.py
@@ -23,7 +23,7 @@ States: Draft → Confirm → 1st Approved (validate1) → 2nd Approved (validat
         'security/ir.model.access.csv',
         'security/security.xml',
         'wizard/hr_leave_refuse_wizard_views.xml',
-        # 'views/hr_employee_views.xml',
+        'views/hr_employee_views.xml',
         'views/hr_leave_views.xml',
         'views/hr_leave_approver_menu.xml',
         'views/hr_leave_summary_views.xml',
