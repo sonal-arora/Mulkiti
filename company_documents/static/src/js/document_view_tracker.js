559c0e345362bd21a01f2fe52b299e55a158c60b
Sonal Arora <sonalarora@Sonals-MacBook-Air.local>
2026-05-13 17:36:37 +0530

    work on documents

--- company_documents/static/src/js/document_view_tracker.js
+++ company_documents/static/src/js/document_view_tracker.js
@@ -1,24 +1,19 @@
 /** @odoo-module **/
 import { FormController } from "@web/views/form/form_controller";
 import { patch } from "@web/core/utils/patch";
-import { useService } from "@web/core/utils/hooks";
 
 patch(FormController.prototype, {
-    setup() {
-        super.setup(...arguments);
-        this.rpc = useService("rpc");
-    },
-
     async onWillStart() {
         await super.onWillStart(...arguments);
-        if (
-            this.props.resModel === "company.document" &&
-            this.props.resId
-        ) {
+        if (this.props.resModel === "company.document" && this.props.resId) {
             try {
-                await this.rpc("/company-documents/view/" + this.props.resId, {});
+                await fetch("/company-documents/view/" + this.props.resId, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ jsonrpc: "2.0", id: 1, method: "call", params: {} }),
+                });
             } catch (_e) {
-                // ignore tracking errors
+                // ignore tracking errors silently
             }
         }
     },
